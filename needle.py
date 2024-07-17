7332675a08e90c96e98429c5038521f0d8ba2c30
Ryan Webb <dzanfox@gmail.com>
2024-07-17 10:57:17 -0500

    added more comments

--- needle.py
+++ needle.py
@@ -1,15 +1,31 @@
-def strStr(haystack:str, needle:str) -> str:
-    indexes = [i for i in range(len(haystack)) if haystack.startswith(needle,i)]
-    if indexes:
-        strIndexes = (str(element) for element in indexes)
-        separator =", "
-        result = separator.join(strIndexes)
-        return result
-    else:
-        return 0
+# taken from youtube short video, seems very convoluted for what needs to be done
+# uses the two pointer approach
+
+# def strStr(haystack: str, needle: str) -> int:
+#     for i in range(len(haystack)):
+#         j = 0
+#         while j < len(needle) and i+j < len(haystack) and needle[j] == haystack[i+j]:
+#             j += 1
+#         if j == len(needle):
+#             return i
+#     return -1
+
+# finds substring throughout string if there are multiple occurances
 
 # def strStr(haystack:str, needle:str) -> str:
-#     return haystack.index(needle)
+#     indexes = [i for i in range(len(haystack)) if haystack.startswith(needle,i)]
+#     if indexes:
+#         strIndexes = (str(element) for element in indexes)
+#         separator =", "
+#         result = separator.join(strIndexes)
+#         return result
+#     else:
+#         return 0
 
-print(strStr("The fox jumps over the fence to chase the rabbit","o"))
-    
+# returns only the index of first occurance of substring
+
+def strStr(haystack:str, needle:str) -> str:
+    return haystack.index(needle)
+
+# print(strStr("The fox jumps over the fence to chase the rabbit","o"))
+print(strStr("abcyesxyz", "yes")) 
